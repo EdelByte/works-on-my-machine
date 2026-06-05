86f6ee0adfcb45fef9935450127dd4acdee7125c
EdelByte <edel-byte@proton.me>
2026-06-05 18:55:24 +0200

    bash settings

--- bashrc.d/personal.sh
+++ bashrc.d/personal.sh
@@ -1,3 +1,15 @@
+# Shell defaults
+HISTCONTROL=ignoreboth
+shopt -s cdspell
+shopt -s direxpand
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# Grep
+alias grep='grep --color=auto'
+
 # Custom Git Aliases
 gg() { git add --all && git commit -m "$1" && git push; }
 ggg() { git add --all && git commit -m "$1" && git push; }
